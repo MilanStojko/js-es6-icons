6ca77144f9ed140fffa45e45c9e304875f3559b7
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-01-13 17:00:02 +0100

    problemi con la funzione getBoxCode

---
+++ assets/js/data.js
@@ -0,0 +1,142 @@
+const icone = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const contenitore = document.getElementById("icons-container");
+const filtro = document.getElementById ("icons-filter");
+
+filtro.addEventListener("change", function(){
+	contenitore.innerHTML = "";
+	getBoxesCode(this.value);
+})
+
+getBoxesCode(filtro.value);
+
+function getBoxesCode (filtro){
+	icone.forEach((elemento) =>{
+		console.log(elemento)
+		if((elemento.type == filtro) || (filtro == "all")) {
+			contenitore.innerHTML += getBoxCode(elemento);
+		}
+	});
+}
+
+function getBoxCode(icona){
+	const {name, prefix, family, color} = icona;
+	console.log(icona);
+		`<div class="box">
+		<i class= "${family} ${prefix} ${name}" style="${color}"></i>
+		<div class"">${name}</div>
+		</div>`;
+	}
